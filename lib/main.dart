4d4d0c761915c79bb257ea7fe4105b3204df68bb
Javier Roncallo <jroncallo96@gmail.com>
2020-07-23 19:04:47 -0500

    Add the input to write the text to encrypt

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp(
+    appName: 'My encryptor',
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final String appName;
+
+  MyApp({Key key, this.appName}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: appName),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,68 +34,84 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String _encryptedText = '';
+  String _alphabet = '\\ \'/,.-<>?"+*[]{}qwertyuiopasdfghjklñzxcvbnm';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            _getTextToEncryptField(),
+            _getEncryptedTextWidget()
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  // Widgets
+  Widget _getTextToEncryptField() {
+    return SizedBox(
+      width: 500.0,
+      height: 100.0,
+      child: TextField(
+          decoration: InputDecoration(
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.greenAccent, width: 2.0),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.red, width: 2.0),
+            ),
+            hintText: 'text to encrypt',
+          ),
+          onChanged: _onTextToEncryptChanged),
+    );
+  }
+
+  Widget _getEncryptedTextWidget() {
+    return SizedBox(width: 500.0, height: 100.0, child: Text(_encryptedText));
+  }
+
+  // Level 1
+  void _onTextToEncryptChanged(text) {
+    setState(() {
+      _encryptedText = _encrypt(text);
+    });
+  }
+
+  // Level 2
+  String _encrypt(String text) {
+    return _encryptWithCesar(_alphabet, text, 3);
+  }
+
+  // Level 3
+  String _encryptWithCesar(String alphabet, String text, int increment) {
+    String encryptedText = '';
+    bool invalidCharFound = false;
+    _iterateString(text, (String char) {
+      int index = _alphabet.indexOf(char);
+      if (index < 0) invalidCharFound = true;
+      int newIndex = (index + increment) % alphabet.length;
+      if (!invalidCharFound) {
+        encryptedText += alphabet[newIndex];
+      }
+    });
+    return invalidCharFound ? '' : encryptedText;
+  }
+
+  // Level 4
+  void _iterateString(String str, Function callback) {
+    List<int> stringIndexList =
+        new List<int>.generate(str.length, (int index) => index);
+    stringIndexList.forEach((index) {
+      var char = str[index];
+      callback(char);
+    });
   }
 }
